97768026b92e9f1590c6ad91e5056951c47d2011
Hotline7 <aswinkummaran@gmail.com>
2026-06-17 22:45:50 +0800

    Add bitboard representation for initial chess position

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+namespace ChessConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This represents a white pawn on e2 (Bit 12) and a White Knight on b1 (Bit 1)
+            // Bit 12 = 1 << 12 (value 4096)
+            // Bit 1 = 1 << 1 (value: 2)
+            // Combined value = 4098
+            ulong sampleBitboard = 4098UL;
+
+            Console.WriteLine("Visualising sample bitboard");
+            PrintBitBoard(sampleBitboard);
+        }
+
+        public static void PrintBitBoard(ulong bitboard)
+        {
+            // Loop from rank 8 (index 7) down to Rank 1 (index 0)
+            for (int rank = 7; rank >=0; rank--)
+            {
+                // Print the rank number on the left side
+                Console.Write($"{rank + 1} ");
+
+                for (int file = 0; file < 8; file++)
+                {
+                    // Convert 2D coordinates (rank, file) into 1D bit index (0 to 63)
+                    int squareIndex = rank * 8 + file;
+
+                    // create a mask with 1 at specific square index
+                    ulong mask = 1UL << squareIndex;
+
+                    // Use bitwise AND to check if the bit has 1 in the bitboard
+                    if ((bitboard & mask) != 0)
+                    {
+                        Console.Write("X ");
+                    }
+                    else
+                    {
+                        Console.Write(". ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            // Print the file letters at the bottom
+            Console.WriteLine("\n a b c d e f g h");
+        }
+    }
+}
